ca62c7293f349bb61c9eb1393a3df2a65de1e4b7
heng <1792896577@qq.com>
2022-10-13 15:48:31 +0800

    项目初始化 - 引入了Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <!-- 路由的出口 -->
     <router-view/>
     <h1>程序员头条</h1>
+    <div>
+      <i class="iconfont icon-dianzan2"></i>
+    </div>
+    <div>
+      <van-button type="primary" size="small">主要按钮</van-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 按需引入vant
+import { Button } from 'vant'
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
